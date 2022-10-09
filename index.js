ee48ce41e784cb76fd9995abb4173819d0654979
Daniflav94 <danyelly_mell@hotmail.com>
2022-10-09 17:49:19 -0300

    Exclusão de tarefas corrigido

--- index.js
+++ index.js
@@ -1,55 +1,61 @@
 //DOM representa o html dentro do javascript
 
+const todos = []
+const importanciaIcones = []
+if(localStorage.length > 0) {
+    JSON.parse(localStorage.getItem('todosArray'));
+    JSON.parse(localStorage.getItem('iconeImportancia')); 
+    renderizarTodos() 
+}
+
 const form = document.getElementById('todo-form')
 
-const todos = []
-const bolinhas = []
-
-let importante //query selector para pegar todos os inputs do tipo "radio".
-let medio
-let facil
+let importante
+let moderado
+let semUrgencia
 let selectedSize
 let icone
 
 form.addEventListener('submit', function(evento){ //função anonima
 
     importante = document.querySelector(".importante")
-    medio = document.querySelector(".medio")
-    facil = document.querySelector(".facil")
+    moderado = document.querySelector(".moderado")
+    semUrgencia = document.querySelector(".semUrgencia")
 
     evento.preventDefault() //cancela o comportamento padrão de um formulario que seria o recarregamento da página 
     evento.stopPropagation() //evita que a emissão do evento se propague para outros elementos
     const input = document.querySelector('#todo')
-
-    
+   
 
     if(importante.checked) {
         selectedSize = importante.value
         icone = document.createElement('span')
         icone.classList.add('material-symbols-outlined', 'iconeVermelho')
         icone.innerText = 'circle'
-        bolinhas.push(icone)
+        importanciaIcones.push(icone)
            
     
-    } else if (medio.checked){
-        selectedSize = medio.value
+    } else if (moderado.checked){
+        selectedSize = moderado.value
         icone = document.createElement('span')
         icone.classList.add('material-symbols-outlined', 'iconeAmarelo')
         icone.innerText = 'circle'
-        bolinhas.push(icone)
+        importanciaIcones.push(icone)
         
        
-    } else if (facil.checked){
-        selectedSize = facil.value
+    } else if (semUrgencia.checked){
+        selectedSize = semUrgencia.value
         icone = document.createElement('span')
         icone.classList.add('material-symbols-outlined', 'iconeVerde')
         icone.innerText = 'circle' 
-        bolinhas.push(icone)
-        
+        importanciaIcones.push(icone)     
     }
     
     todos.push(input.value)
-    
+    input.value = ''
+
+    localStorage.setItem('todosArray', JSON.stringify(todos))
+    localStorage.setItem('iconeImportancia', JSON.stringify(importanciaIcones))
 
     renderizarTodos()
 }) 
@@ -80,9 +86,15 @@ function renderizarTodos(){
         
         //arrow function, sempre são anonimas
         btnDelete.addEventListener('click', () => {
-            const index = todos.indexOf(todos[i])
-            todos.splice(index, 1) //remove o elemento
+            
+            todos.splice(i, 1) //remove o elemento
+            importanciaIcones.splice(i,1)
+
+            localStorage.setItem('todosArray', JSON.stringify(todos))
+            localStorage.setItem('iconeImportancia', JSON.stringify(importanciaIcones))
+
             renderizarTodos()
+            
         }) 
 
         const spanIcon = document.createElement('span')
@@ -90,12 +102,10 @@ function renderizarTodos(){
         spanIcon.innerText = 'delete'
 
         btnDelete.appendChild(spanIcon) //coloca novos elementos HTML dentro de outros
-        divCardBody.append(pTodosText, bolinhas[i], btnDelete) //append coloca mais de um
+        divCardBody.append(pTodosText, importanciaIcones[i], btnDelete) //append coloca mais de um
         divCard.appendChild(divCardBody)
         todosListSection.appendChild(divCard)  
-
-
     }
-    
-   
+
+    importanciaIcones.innerHTML  = ''   
 }
